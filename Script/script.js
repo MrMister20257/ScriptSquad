a166f6e71bc17db8459d70be75dbfc91d19b828a
MrMister20257 <norman.a.marin@gmail.com>
2024-10-03 00:15:17 +0200

    diverse änderungen

--- Script/script.js
+++ Script/script.js
@@ -4,10 +4,15 @@ function topFunction() {
 }
 
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
+}
+
+function launchRocket() {
+  let rocket = document.getElementById("rocket");
+  rocket.classList.add("launch");
 }
