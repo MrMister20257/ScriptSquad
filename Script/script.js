9b3a964c74dea9b7fa649d2627401efcd0f85a53
MrMister20257 <norman.a.marin@gmail.com>
2024-10-02 20:52:48 +0200

    auskommentiter

--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,15 @@
-let mybutton = document.getElementById("fixed-button");
+//let mybutton = document.getElementById("fixed-button");
 
 
-window.onscroll = function() {scrollFunction()};
+//window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+//function scrollFunction() {
+//  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//    mybutton.style.display = "block";
+//  } else {
+//    mybutton.style.display = "none";
+//  }
+//}
 
 function topFunction() {
   document.body.scrollTop = 0;
