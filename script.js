63323d5029c128f016b29b25977d36b4eb7a9f22
MrMister20257 <norman.a.marin@gmail.com>
2024-10-02 14:01:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let mybutton = document.getElementById("fixed-button");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
